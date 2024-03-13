9c82eefd94e964b532962fabe65799faf8d48e92
stella180303 <stelladdvnx1803@gmail.com>
2024-03-13 22:33:52 +0700

    menambahkan file latihan react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+
 import logo from './logo.svg';
 import './App.css';
-
+import Hello from './Components/Hello';
+import Welcome from './Components/Welcome';
+import { Component } from 'react';
+import Message from './Components/Message';
+import Counter from './Components/Counter';
+import FunctionClick from './Components/FunctionClick';
+import ClassClick from './Components/ClassClick';
+import EventBind from './Components/EventBind';
+import ParentComponent from './Components/ParentComponent';
+import UserGreeting from './Components/UserGreeting';
+import NameList from './Components/NameList';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    
+    <NameList />
+    
+
+     {/* 
+     <UserGreeting />
+    <ParentComponent />
+    <EventBind />
+    <FunctionClick />
+    <ClassClick />
+    <Counter />
+    <h1>State</h1>
+    <Message /> 
+     functional component
+      <Welcome  />
+      <Hello  name = "Bruce" heroname="guru">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is children props
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </Hello>
+      
+      <Hello  name = "Lala" heroname="polisi">
+        <button>Action</button>
+      </Hello>
+
+    class component
+      <Welcome  name = "Diana" heroname="suster" />
+      <Welcome  name = "sicantik" heroname="suster" /> */}
+    
+
+ 
+      </div> 
+ );
 }
 
 export default App;
---
+++ src/Components/EventBind.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+  
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       message: 'hello'
+    }
+    //bind  di bagian constructor
+    // this.clickHandler = this.clickHandler.bind(this)
+  }
+
+//   clickHandler(){
+//     //untuk mengubah state,pake set state
+//     this.setState({
+//         message:'goodbye'
+//     })
+//   }
+
+    clickHandler =() =>{
+        this.setState({
+            message:'goodbye'
+        })
+    }
+  
+
+    render() {
+    return (
+      <div>
+        <p>{this.state.message}</p>
+        {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
+        {/* <button onClick={() => this.clickHandler()}>Click</button> */}
+        <button onClick={this.clickHandler}>Click</button>
+      </div>
+    )
+  }
+}
+
+export default EventBind

1d7bfa7d1541b26ddc6da27eba8b35af2a796d6b
stella180303 <stelladdvnx1803@gmail.com>
2024-03-17 21:28:12 +0700

    menambahkan file life cycling methods

---
+++ src/Components/LifecycleB.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+ class LifecycleB extends Component {
+    
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name: 'Stella'
+      }
+      console.log('LifecycleB Constructor')
+    }
+    
+    static getDerivedStateFromProps(props,state){
+        console.log('LifecycleB getDerivedStateFromProps')
+        return null
+    }
+  
+    componentDidMount(){
+        console.log('LifecycleB componentDidMount')
+    }
+    
+    
+    render() {
+
+        console.log('LifecycleB render')
+
+        return (
+        <div>
+            LifecycleB
+        </div>
+        )
+  }
+}
+
+export default LifecycleB

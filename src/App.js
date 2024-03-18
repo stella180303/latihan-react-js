41e16c1b3a2b3a65836e014f062661dc423038ad
stella180303 <stelladdvnx1803@gmail.com>
2024-03-18 20:51:47 +0700

    mengupdate App.js

--- src/App.js
+++ src/App.js
@@ -27,16 +27,62 @@ import ParentComp from './Components/ParentComp';
 import RefsDemo from './Components/RefsDemo';
 import Focusinput from './Components/Focusinput';
 import FRParentInput from './Components/FRParentInput';
+import PortalDemo from './Components/PortalDemo';
+import Hero from './Components/Hero';
+import ErrorBoundary from './Components/ErrorBoundary';
+import ClickCounter from './Components/ClickCounter';
+import HoverCounter from './Components/HoverCounter';
+import ClickCounterTwo from './Components/ClickCounterTwo';
+import HoverCounterTwo from './Components/HoverCounterTwo';
+import User from './Components/User';
+import CounterHandler from './Components/CounterHandler';
+import ComponentC from './Components/ComponentC';
+import { UserProvider } from './Components/UserContext';
 
 function App() {
   return (
     <div className="App">
     
+    <UserProvider value="Stella">
+      <ComponentC />
+    </UserProvider>
     
-    <FRParentInput />
-   
-
      {/* 
+
+    Render Props :
+    <CounterHandler 
+    render={(count, incrementCount) => <ClickCounterTwo count={count} incrementCount={incrementCount} />} 
+    />
+
+    <CounterHandler 
+    render={(count, incrementCount) => <HoverCounterTwo count={count} incrementCount={incrementCount} />} 
+    />
+
+    <ClickCounterTwo />
+    <HoverCounterTwo />
+    <User render= {(isLoggedIn) => isLoggedIn ? 'Stella' : 'Guest'} />
+
+    Higher Order Components : 
+    -<HoverCounter />
+    - <ClickCounter name='Stella'  />
+
+    Error Boundary :
+    <ErrorBoundary>
+     <Hero heroName="Batman" />
+    </ErrorBoundary>
+
+    <ErrorBoundary>
+      <Hero heroName="Superman" />
+    </ErrorBoundary>
+    
+    <ErrorBoundary>
+      <Hero heroName="Joker" />
+    </ErrorBoundary>
+
+
+    Portal : <PortalDemo />
+
+    Forwarding Ref : <FRParentInput />
 
     Refs (Class component) :<Focusinput />
 
@@ -102,10 +148,10 @@ function App() {
 
     class component
       <Welcome  name = "Diana" heroname="suster" />
-      <Welcome  name = "sicantik" heroname="suster" /> */}
+      <Welcome  name = "sicantik" heroname="suster" /> */
     
 
- 
+  }
       </div> 
  );
 }
